40dc10e6f2008ecdd5d5ddf4a63e648f5b8e6b83
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2026-04-09 00:28:58 +0800

    feat(order-route): 新增 `/api/orders` 路徑

---
+++ backend/src/routes/orders.js
@@ -0,0 +1,36 @@
+import { Router } from "express";
+import multer from "multer";
+import { decodeJwt } from "jose";
+
+import db from "../db.js";
+
+const router = Router();
+const formHandler = multer();
+
+router.post("/", formHandler.none(), async (req, res) => {
+   const body = req.body;
+   if (!body || !body.token) {
+      return res.sendStatus(400);
+   }
+   const token = body.token;
+   const { roles } = await decodeJwt(token);
+   if (!roles.includes("customer")) {
+      return res.sendStatus(403);
+   }
+   const cart = JSON.parse(body.cart);
+   const address = body["postal-code"] + body["city"] + body["district"] + body["street-address"];
+   const order = await db.Order.create({
+      shippingAddress: address,
+      recipientName: body["recipient-name"],
+      recipientEmail: body["recipient-email"],
+      recipientPhone: body["recipient-phone"],
+   });
+   for (const item of cart) {
+      console.log(item);
+      await order.addItem(item.id, item.quantity, parseInt(item.price));
+   }
+   console.log(order.toJSON());
+   return res.sendStatus(200);
+});
+
+export default router;
