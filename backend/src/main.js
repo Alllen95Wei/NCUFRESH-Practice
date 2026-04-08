73eba29fe42f56f1b12432c01017df66c7bb4012
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2026-04-09 00:30:15 +0800

    feat(order-route): 新增 `/api/orders` 路徑

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,9 +1,10 @@
 import express from "express";
 // DB
-// import db from "./db.js";
+import db from "./db.js";
 // Routes
 import authRouter from "./routes/auth.js";
 import productsRouter from "./routes/products.js";
+import ordersRouter from "./routes/orders.js";
 // Middlewares
 import logger from "./middlewares/logger.js";
 
@@ -17,7 +18,10 @@ app.use(logger);
 
 apiRouter.use("/auth", authRouter);
 apiRouter.use("/products", productsRouter);
+apiRouter.use("/orders", ordersRouter);
 
 app.use("/api", apiRouter);
 
 app.listen(3030);
+
+// await db.sequelize.sync({ alter: true });
