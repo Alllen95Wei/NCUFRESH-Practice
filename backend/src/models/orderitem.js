e33fc464e08861c4de8a6769bec2a755c41b133f
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2026-04-09 23:38:15 +0800

    feat(orderitem): 完成模型關聯

--- backend/src/models/orderitem.js
+++ backend/src/models/orderitem.js
@@ -1,7 +1,17 @@
 import { DataTypes, Model } from "sequelize";
 
 class OrderItem extends Model {
-    // static associate(models) {}
+    static associate(models) {
+        OrderItem.belongsTo(models.Order, {
+            foreignKey: "orderId",
+            as: "order",
+        });
+
+        OrderItem.belongsTo(models.Product, {
+            foreignKey: "productId",
+            as: "product",
+        });
+    }
 }
 
 function initModel(sequelize) {
